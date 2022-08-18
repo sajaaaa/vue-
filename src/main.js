32b9c3c1e72e35bf3266bb514e50414a4e6b2e2b
“阿杰” <3069787439@qq.com>
2022-08-18 10:26:34 +0800

    完成新增事项和删除事项功能

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './pages/todoList.vue'
 import ViewUIPlus from 'view-ui-plus'
 import 'view-ui-plus/dist/styles/viewuiplus.css'
+// import { Button, Table } from 'view-ui-plus';
+// app.component('Button', Button);
+// app.component('Table', Table);
 // import './assets/main.css'
 const app = createApp(App)
 app.use(ViewUIPlus)
--- src/pages/todoList.vue
+++ src/pages/todoList.vue
@@ -2,33 +2,39 @@
     <div class="counter">
         <div class="mainPage">
             <div class="navBar">
-                <div class="dateBar">date</div>
+                <div class="dateBar">
+                    Date
+                    <div class="dateIcon">
+                        <Icon type="md-arrow-dropup" />
+                        <Icon type="md-arrow-dropdown" />
+                    </div>
+                </div>
                 <div class="todoId">#id</div>
                 <div class="descriptionBar">description</div>
                 <div class="operationBar">operation</div>
             </div>
             <div class="content">
                 <div :class="'content' + content.id" v-for="(content) in contents" :key="content.id">
-                    <div :class="'content' + content.id + 'date'" :style="{ 'width': '20vw' }">
-                        {{ content.date }}
-                    </div>
+                    <Input :class="'content' + content.id + 'date'" :border="false" @on-focus="onFocus"
+                        :style="{ 'width': '20vw', 'text -align': 'center' }" v-model="content.date">
+                    </Input>
                     <div :class="'content' + content.id + 'id'" :style="{ 'width': '10vw' }">
                         {{ content.id }}
                     </div>
-                    <div :class="'content' + content.id + 'description'" :style="{ 'width': '35vw' }">
-                        {{ content.description }}
-                    </div>
+                    <Input :ref="'descriptionInput' + content.id" :class="'content' + content.id + 'description'"
+                        :style="{ 'width': '35vw', 'text-align': 'center' }" :border="false"
+                        v-model="content.description">
+                    </Input>
                 </div>
             </div>
             <div class="bottomBar">
                 <div class="pagination">
                     Pagination
                 </div>
-                <div class="search">
-                    Search
-                </div>
-                <div class="add">Add+</div>
-                <div class="delete">Delete-</div>
+                <Input class="search" placeholder="Search">
+                </Input>
+                <Button class="add" @click="addTodo">Add+</Button>
+                <Button class="delete" @click="deleteTodo">Delete-</Button>
             </div>
         </div>
     </div>
@@ -36,43 +42,79 @@
 </template>
 
 <script setup>
+import { getCurrentInstance, onMounted } from '@vue/runtime-core'
+import { toRaw } from '@vue/reactivity';
 import { reactive, ref } from 'vue';
+import { Button, Input, Icon } from 'view-ui-plus';
+let currentInstance = '';
+onMounted(() => {
+    currentInstance = getCurrentInstance();
+    console.log(toRaw(currentInstance.ctx.$refs));
+    // console.log(JSON.parse(JSON.stringify(currentInstance.ctx.$refs)));
+
+})
+//测试样例
 const contents = reactive([
     {
         date: "2020/8/14",
-        id: "1",
+        id: 1,
         description: "完成todoList1",
     },
     {
         date: "2020/8/15",
-        id: "2",
+        id: 2,
         description: "完成todoList2",
     },
     {
         date: "2020/8/15",
-        id: "3",
+        id: 3,
         description: "完成todoList3",
     },
     {
         date: "2020/8/15",
-        id: "4",
+        id: 4,
         description: "完成todoList4",
     },
-    {
-        date: "2020/8/15",
-        id: "5",
-        description: "完成todoList5",
-    },
-    {
-        date: "2020/8/15",
-        id: "6",
-        description: "完成todoList6",
-    },
-
+    // {
+    //     date: "2020/8/15",
+    //     id: 5,
+    //     description: "完成todoList5",
+    // },
+    // {
+    //     date: "2020/8/15",
+    //     id: "6",
+    //     description: "完成todoList6",
+    // },
 ]);
 
+// 新增数据时，聚焦到新的输入框
+const onFocus = (number) => {
+    // console.log("focus");
+    const descriptionInputNumber = `descriptionInput${number}`;
+    console.log(descriptionInputNumber);
+    console.log(toRaw(currentInstance.ctx.$refs)[descriptionInputNumber][0]);
+    toRaw(currentInstance.ctx.$refs)[descriptionInputNumber][0].focus({
+        cursor: 'all'
+    });
+    // console.log(e);
+}
+
+// 点击按钮新增数据，此处异步，为了让数组更新后才聚集新输入框
+const addTodo = async () => {
+    const newIndex = contents.length + 1;
+    await contents.splice(newIndex, 0, { data: "", id: newIndex, description: "", });
+    // console.log(contents);
+    onFocus(newIndex);
+}
+// 点击按钮删除数据
+const deleteTodo = () => {
+    const newIndex = contents.length - 1;
+    contents.splice(newIndex, 1); // 删除最后的一个数据
+    console.log(contents);
+    // onFocus(newIndex);
+}
 </script>
 
-<style  lang="scss" scoped>
+<style  lang="scss" >
 @import './todoList.scss';
 </style>
